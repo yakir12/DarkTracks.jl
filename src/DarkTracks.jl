2f37ea76bbaafae01de04a08994431045e2c6675
yakir12 <12.yakir@gmail.com>
2018-10-09 16:03:16 +0200

    streamlined

--- src/DarkTracks.jl
+++ src/DarkTracks.jl
@@ -2,7 +2,7 @@ module DarkTracks
 
 export main
 
-using FFmpegPipe, MappedArrays, Statistics, IdentityRanges, Dates, Images, Unitful, DelimitedFiles, Printf, JSON, ImageDraw
+using FFmpegPipe, OnlineStats, Statistics, IdentityRanges, Dates, Images, Unitful, DelimitedFiles, Printf, JSON#, ImageDraw
 
 function getdimensions(videofile, start_frame, stop_frame)
     str = read(`ffprobe -show_streams -of json -v quiet -i $videofile`, String)
@@ -25,31 +25,20 @@ function warpdimensions(w₀, h₀, fps₀, spatialscale, temporalscale)
     w, h, fps
 end
 
-
-function getimages(videofile, start_second, duration_second, fps, w, h)
-    imgs = Vector{Array{Gray{N0f8}, 2}}()
-    vi = openvideo(videofile, "r", options = (ss = start_second, t = duration_second, r = fps, s = string(w, "x", h), pix_fmt = "gray"))
-    while !eof(vi)
-        push!(imgs, readframe(vi))
-    end
-    close(vi)
-    n = length(imgs)
-    ts = range(0, stop = duration_second, length = n)
-    imgs, ts, n
-end
-
 function detectnext(r, img, noise, ind)
     rows = IdentityRange((-r:r) .+ ind[1])
     cols = IdentityRange((-r:r) .+ ind[2])
-    roi = view(padarray(img, Fill(zero(img[1]), (r, r))), rows, cols)
+    pimg = padarray(img, Fill(zero(img[1]), (r, r)))
+    roi = @view pimg[rows, cols]
     M, ind = findmax(roi)
     M > noise[ind] ? ind : missing
 end
 
-function detectfirst(imgs, noise, sz)
+function detectfirst(vi, noise, sz, n)
     ind₀ = round.(Int, sz./2)
     r₀ = min(sz...) ÷ 11
-    for (i, img) in enumerate(imgs)
+    for i in 1:n
+        img = readframe(vi)
         ind = detectnext(r₀, img, noise, ind₀)
         if !ismissing(ind)
             return i, ind
@@ -58,14 +47,14 @@ function detectfirst(imgs, noise, sz)
     error("failed to detect the beetle's initial starting point!")
 end
 
-
-function track(r₀, imgs, noise, n, sz)
+function track(r₀, vi, noise, n, sz)
     inds = Vector{Union{Missing, CartesianIndex{2}}}(missing, n)
-    i1, ind = detectfirst(imgs, noise, sz)
+    i1, ind = detectfirst(vi, noise, sz, n)
     inds[i1] = ind
     r = copy(r₀)
     for i in i1+1:n
-        inds[i] = detectnext(r, imgs[i], noise, inds[findlast(!ismissing, inds)])
+        img = readframe(vi)
+        inds[i] = detectnext(r, img, noise, inds[findlast(!ismissing, inds)])
         if ismissing(inds[i])
             r += r₀ # TODO
         else
@@ -75,18 +64,16 @@ function track(r₀, imgs, noise, n, sz)
     inds
 end
 
-
-function savedata(videofile, ts, inds, spatialscale)
+function savedata(videofile, ts, inds)
     path, nameext = splitdir(videofile)
     name, _ = splitext(nameext)
     csvname = joinpath(path, "$name.csv")
-    xs = ["x"; spatialscale.*getindex.(inds, 1)]
-    ys = ["y"; spatialscale.*getindex.(inds, 2)]
+    xs = ["x"; getindex.(inds, 2)]
+    ys = ["y"; getindex.(inds, 1)]
     writedlm(csvname, zip(["seconds"; ts], xs, ys))
 end
 
-
-function drawit(videofile, n, inds, imgs)
+#=function drawit(videofile, n, inds, imgs)
     path, nameext = splitdir(videofile)
     name, _ = splitext(nameext)
     imgs2 = joinpath(path, name)
@@ -100,31 +87,42 @@ function drawit(videofile, n, inds, imgs)
         save(joinpath(imgs2, @sprintf "%06i.png" i), img2)
     end
     run(`ffmpeg -y -framerate 25 -i $(joinpath(imgs2, "%06d.png")) $(joinpath(imgs2, "0.mp4"))`)
-end
+end=#
 
-getnoise(x) = 2mean(Float64.(x))
+function getnoise(sz, videofile, start_second, duration_second)
+    h, w = sz
+    o = [Mean() for i in 1:h, j in 1:w]
+    nframes = 9
+    vi = openvideo(videofile, "r", ss_in = start_second, t = duration_second, r = nframes/duration_second, s = string(w, "x", h), pix_fmt = "gray")
+    for i in 1:nframes
+        img = readframe(vi)
+        for j in eachindex(img)
+            fit!(o[j], Float64(img[j]))
+        end
+    end
+    close(vi)
+    2value.(o)
+end
 
 getradius(sscale, tscale) = 10exp(-1.020819402836599log(sscale) + 0.7668247162044156log(tscale) + 0.8641290371601322)
 
 function main(videofile, start_frame, stop_frame,  spatialscale, temporalscale)
-
     w₀, h₀, fps₀, start_second, duration_second = getdimensions(videofile, start_frame, stop_frame)
     w, h, fps = warpdimensions(w₀, h₀, fps₀, spatialscale, temporalscale)
     sz = (h, w)
-    imgs, ts, n = getimages(videofile, start_second, duration_second, fps, w, h)
+    n = round(Int, duration_second*fps)
+    ts = range(0, stop = duration_second, length = n)
     r₀ = round(Int, getradius(spatialscale, temporalscale))
-    o = [[imgs[j][i] for j in 1:(n-1)÷9:n] for i in CartesianIndices(sz)] # TODO
-    noise = mappedarray(getnoise, o);
-    inds = track(r₀, imgs, noise, n, sz)
+    noise = getnoise(sz, videofile, start_second, duration_second)
+    vi = openvideo(videofile, "r", ss_in = start_second, t = duration_second, r = fps, s = string(w, "x", h), pix_fmt = "gray")
+    inds = track(r₀, vi, noise, n, sz)
+    close(vi)
+
     tss = [t for (t, ind) in zip(ts, inds) if !ismissing(ind)]
     indss = [spatialscale.*Tuple(ind) for ind in inds if !ismissing(ind)]
-
-    # drawit(videofile, n, inds, imgs)
-
-    # savedata(videofile, ts, inds, spatialscale)
-
+    savedata(videofile, tss, indss)
     tss, indss
-
+    # drawit(videofile, n, inds, imgs)
 end
 
 
