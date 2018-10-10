25126c86fbbad4283f1fc0b343db5cbcbba275a5
yakir12 <12.yakir@gmail.com>
2018-10-10 14:06:19 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using DarkTracks
 using Test
 
-videofile = joinpath(@__DIR__(), "testvideo.mp4")
+videofile = joinpath(@__DIR__(), "..", "test", "testvideo.mp4")
 ts, inds = main(videofile, stop_frame = 100, spatialscale = 10)
 
 @test ts == [1.32, 1.54, 1.76, 1.98]
-@test inds = [(510, 730), (520, 730), (520, 730), (520, 730)]
+@test inds == [(510, 730), (520, 730), (520, 730), (520, 730)]
